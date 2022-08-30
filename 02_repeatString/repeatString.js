652ebed1a91b3082b90167ba9908e517668d3f87
Nimb <erik.nimb@wsu.edu>
2022-08-29 19:59:46 -0700

    1,2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    var output = '';
+    if (num < 0){
+        return "ERROR";
+    }
 
+    for (var i = 0; i < num; i++){
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
