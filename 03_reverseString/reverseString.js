f06ebfca6b36f6ac12067cd7244d6c0ff18c09f0
Nimb <erik.nimb@wsu.edu>
2022-08-30 18:44:23 -0700

    completed reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    
+    var l = 0;
+    var r = string.length-1;
+    const newString = [];
+    
+    if (string == ""){
+        return "";
+    }
+    for(let i = r; i>=0;i--){
+        newString.push(string[i]);
+    }
+    
+    return newString.join('');
 };
 
 // Do not edit below this line
