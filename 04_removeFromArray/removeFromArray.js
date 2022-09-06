3dff94b58509769a24065dca21955c9b357a2e81
Nimb <erik.nimb@wsu.edu>
2022-09-06 14:53:59 -0700

    completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
 const removeFromArray = function() {
+    var theArray = arguments[0];
+    var newArray = [];
+    var numVars = arguments.length-1;
+    
+    for (var j = 0; j <= theArray.length-1; j++){
+        for (var i = 1; i <= numVars; i++){
+            if (theArray[j] != arguments[i]){
+                newArray.push(theArray[j]);
+                theArray.shift()
+            }
+            console.log(theArray)
+            console.log(newArray)
+        }
+    }
 
+    
+    
+    
+    return newArray;
 };
 
 // Do not edit below this line
